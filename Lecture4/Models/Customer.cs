516f4ecd0987cf092ab5b6dd8a729e75c5272261
Scott Kim <88755162+rubyonsoft@users.noreply.github.com>
2023-01-15 05:24:21 +0900

    Add files via upload

---
+++ Lecture4/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Northwind7.Models
+{
+    public class Customer
+    {
+        public string? CustomerID { get; set; }
+        public string? CompanyName { get; set; }
+        public string? ContactName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ Lecture4/Models/DataViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Rubyon.NetFramework;
+
+namespace Northwind7.Models
+{
+	public class DataViewModel
+	{
+		public string? GridData { get; set; }
+		public GridDataTable? GridTable { get; set; }
+		public GridDataTable? GridTable2 { get; set; }
+		public GridDataTable? GridTable3 { get; set; }
+	}
+}
